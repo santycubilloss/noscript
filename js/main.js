a545fe93deed931cd2ee63bae6041150d157d69f
santycubilloss <92126220+santycubilloss@users.noreply.github.com>
2021-11-30 17:29:52 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,8 +1,2 @@
-/*alert("JavaScript Habilitado");*/
- Swal.fire({
-   icon: 'error',
-   title: 'Oops...',
-   html: '<b>Usuario y/o contraseña incorrectos</b>',
-   footer: '<a href="#" class"Login">¿Olvido su contraseña?</a>'
-})
+alert("JavaScript Habilitado");
 window.location.replace("http://santyswebsite.epizy.com/");
